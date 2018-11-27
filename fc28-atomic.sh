db45cd05831a8a90d20846f749751b56e40260d0
bl4ckout31 <bl4ckout31@gmail.com>
2018-11-27 16:09:53 +0100

    Add fc28-atomic docker config

--- fc28-atomic.sh
+++ fc28-atomic.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+cat > /etc/docker/daemon.json << EOF
+{
+        "log-driver": "journald",
+        "data-root": "/dockyard",
+        "storage-driver": "overlay2",
+        "storage-opts": [
+                "overlay2.override_kernel_check=true"
+        ],
+        "mtu": 1450,
+        "bip": "172.17.0.1/24",
+        "fixed-cidr": "172.17.0.0/24",
+        "insecure-registries" : ["localhost:5000"]
+}
+EOF
+
 sed -i -e 's/--live-restore//g' /etc/sysconfig/docker
 systemctl restart docker
 
