755977abd1f76d5f4aaa646091dcc0ad8759d7e2
bl4ckout31 <bl4ckout31@gmail.com>
2018-12-02 14:56:06 +0100

    Remove nginx files fetching

--- start-swarm.sh
+++ start-swarm.sh
@@ -21,19 +21,6 @@ echo "Fetching docker-swarm token and IP..."
 scp -q centos@$1:~/swarm-join.txt .
 scp -q centos@$1:~/ip_manager.txt .
 
-res=1
-echo "Waiting for Nginx files..."
-while [ $res -ne 0 ]; do 
-    sleep 1
-    ssh -q centos@$1 "while [ ! -f /tp-vcc/passwd ]; do sleep 1; done"
-    res=$?
-done
-
-echo "Fetching Nginx files..."
-scp -q centos@$1:/tp-vcc/ngnix.crt .
-scp -q centos@$1:/tp-vcc/ngnix.key .
-scp -q centos@$1:/tp-vcc/passwd .
-
 echo "Booting workers..."
-nova boot --min-count 3 --max-count 3 --flavor m1.small --image FC28atomic --nic net-id=c1445469-4640-4c5a-ad86-9c0cb6650cca --security-group default --meta ip_manager="$(cat ip_manager.txt)" --meta swarm-join="$(cat swarm-join.txt)" --user-data fc28-atomic.sh --key-name mykey FC28atomic_${OS_USERNAME}
+nova boot --min-count 1 --max-count 1 --flavor m1.small --image FC28atomic --nic net-id=c1445469-4640-4c5a-ad86-9c0cb6650cca --security-group default --meta ip_manager="$(cat ip_manager.txt)" --meta swarm-join="$(cat swarm-join.txt)" --user-data fc28-atomic.sh --key-name mykey FC28atomic_${OS_USERNAME}
 echo "Done!"
