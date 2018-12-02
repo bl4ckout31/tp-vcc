8b6befebb32d62955ef658427929356f792581d7
bl4ckout31 <bl4ckout31@gmail.com>
2018-12-02 16:49:13 +0100

    Images

--- cos75.sh
+++ cos75.sh
@@ -23,26 +23,27 @@ cat > /etc/docker/daemon.json << EOF
         "mtu": 1450,
         "bip": "172.17.0.1/24",
         "fixed-cidr": "172.17.0.0/24",
-        "insecure-registries" : ["localhost:5000"]
+        "insecure-registries" : ["$ip_addr:5000"]
 }
 EOF
 
 echo $ip_addr > /home/centos/ip_manager.txt
 systemctl enable --now docker
-docker service create --name registry --p 5000:5000 registry 
-#docker run -d --restart=always -p 5000:5000 --name registry registry
+docker run -d --restart=always -p 5000:5000 --name registry registry:2
 
 yum install -y docker-compose git httpd-tools
 git clone https://github.com/bl4ckout31/tp-vcc.git /tp-vcc
 
 cd /tp-vcc/app
-docker build -t localhost:5000/django-app:latest .
-docker push localhost:5000/django-app:latest
+docker build -t django-app:latest .
+docker tag django-app:latest $ip_addr:5000/django-app:latest
+docker push $ip_addr:5000/django-app:latest
 
 cd /tp-vcc/proxy
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FR/ST=Toulouse/L=Toulouse/O=Université Paul Sabatier/CN=tp-vcc" -keyout nginx.key -out nginx.crt
 htpasswd -cb passwd "tp-vcc" "789456123"
-docker build -t localhost:5000/proxy:latest .
-docker push localhost:5000/proxy:latest
+docker build -t proxy:latest .
+docker tag proxy:latest $ip_addr:5000/proxy:latest
+docker push $ip_addr:5000/proxy:latest
 
 docker swarm init --advertise-addr $ip_addr | grep "$ip_addr" | sed -e "s/[[:space:]]\+/ /g" > /home/centos/swarm-join.txt
--- fc28-atomic.sh
+++ fc28-atomic.sh
@@ -14,12 +14,17 @@ cat > /etc/containers/registries.conf << EOF
 registries = ['docker.io', 'registry.fedoraproject.org', 'quay.io', 'registry.access.redhat.com']
 
 [registries.insecure]
-registries = ['localhost:5000']
+registries = ['$ip_manager:5000']
 
 [registries.block]
 registries = []
 EOF
 
 systemctl restart docker
+
+docker pull $ip_manager:5000/proxy:latest
+docker pull $ip_manager:5000/django-app:latest
+docker tag $ip_manager:5000/proxy:latest proxy:latest
+docker tag $ip_manager:5000/django-app:latest django-app:latest
 
 cat /tmp/meta_data.json | python -m json.tool | grep swarm-join | cut -d '"' -f 4 | sh
